23c4f475c73bdf0f44f90b90efdc82c9403466c9
kburts <kevin.burton2@gmail.com>
2014-05-18 18:57:58 -0700

    Added serializers from django_playlist repo.

--- music/playlist/serializers.py
+++ music/playlist/serializers.py
@@ -9,3 +9,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
+
+class SongSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Song
+        fields = ('name', 'playlist', 'url')
+
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    songs = SongSerializer(many=True)
+
+    class Meta:
+        model = Playlist
+        fields = ('title', 'user', 'songs' 'id')
