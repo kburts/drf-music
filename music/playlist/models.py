7f6756291754051ef304f98ac67980e05468eaf1
kburts <kevin.burton2@gmail.com>
2014-05-19 00:47:48 -0700

    Updated models to include FK's to users and updated Playlist model to include creation date and modified date which are updated with its save function.

--- music/playlist/models.py
+++ music/playlist/models.py
@@ -1,23 +1,35 @@
+import datetime
+
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Playlist(models.Model):
     title = models.CharField(max_length=200)
-    creation_date = models.DateTimeField('date created', default=timezone.now())
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
     user = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        # http://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add
+        if not self.id:
+            self.created = datetime.datetime.today()
+        self.modified = datetime.datetime.today()
+        return super(User, self).save(*args, **kwargs)
 
 class Song(models.Model):
+    playlist = models.ForeignKey(Playlist)
+
     name = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
-    playlist = models.ForeignKey(Playlist)
     url = models.URLField(max_length=200)
+
+    added_by = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.name
