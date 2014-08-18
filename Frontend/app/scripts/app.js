2530e01ce027ebc55cac9ac1c6ff46e61b937dd7
kburts <kevin.burton2@gmail.com>
2014-08-17 17:42:30 -0700

    Updated app.js to follow style guides: https://github.com/toddmotto/angularjs-styleguide

--- Frontend/app/scripts/app.js
+++ Frontend/app/scripts/app.js
@@ -1,48 +1,58 @@
-(function() {
-  'use strict';
-  var app;
+'use strict';
+var app;
 
-  app = angular.module('playlistApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'api']);
+angular.module('playlistApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute', 'api']);
 
-  app.run(function() {
-    var firstScriptTag, tag;
-    tag = document.createElement('script');
-    tag.src = "http://www.youtube.com/iframe_api";
-    firstScriptTag = document.getElementsByTagName('script')[0];
-    return firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function run() {
+  var firstScriptTag, tag;
+  tag = document.createElement('script');
+  tag.src = "http://www.youtube.com/iframe_api";
+  firstScriptTag = document.getElementsByTagName('script')[0];
+  return firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+};
+
+angular
+  .module('playlistApp')
+  .run(run);
+
+function config($routeProvider, $httpProvider) {
+  $routeProvider.when('/yo', {
+    templateUrl: 'scripts/main/main.html',
+    controller: 'MainCtrl'
+  }).when('/', {
+    templateUrl: 'scripts/playlist/PlaylistListView.html',
+    controller: 'PlaylistListCtrl'
+  }).when('/play', {
+    templateUrl: 'scripts/playlist/PlaylistListView.html',
+    controller: 'PlaylistListCtrl'
+  }).when('/play/create', {
+    templateUrl: 'scripts/playlist/PlaylistFormView.html',
+    controller: 'PlaylistFormCtrl'
+  }).when('/play/:playlistId', {
+    templateUrl: 'scripts/playlist/PlaylistView.html',
+    controller: 'PlaylistCtrl'
+  }).when('/songs', {
+    templateUrl: 'scripts/song/SongListView.html',
+    controller: 'SongListCtrl'
+  }).when('/songs/create', {
+    templateUrl: 'scripts/song/SongFormView.html',
+    controller: 'SongFormCtrl'
+  }).when('/auth', {
+    templateUrl: 'scripts/auth/AuthView.html',
+    controller: 'AuthCtrl'
+  }).otherwise({
+    redirectTo: '/'
   });
+};
 
-  app.config(function($routeProvider, $httpProvider) {
-    $routeProvider.when('/yo', {
-      templateUrl: 'scripts/main/main.html',
-      controller: 'MainCtrl'
-    }).when('/', {
-      templateUrl: 'scripts/playlist/PlaylistListView.html',
-      controller: 'PlaylistListCtrl'
-    }).when('/play', {
-      templateUrl: 'scripts/playlist/PlaylistListView.html',
-      controller: 'PlaylistListCtrl'
-    }).when('/play/create', {
-      templateUrl: 'scripts/playlist/PlaylistFormView.html',
-      controller: 'PlaylistFormCtrl'
-    }).when('/play/:playlistId', {
-      templateUrl: 'scripts/playlist/PlaylistView.html',
-      controller: 'PlaylistCtrl'
-    }).when('/songs', {
-      templateUrl: 'scripts/song/SongListView.html',
-      controller: 'SongListCtrl'
-    }).when('/songs/create', {
-      templateUrl: 'scripts/song/SongFormView.html',
-      controller: 'SongFormCtrl'
-    }).when('/auth', {
-      templateUrl: 'scripts/auth/AuthView.html',
-      controller: 'AuthCtrl'
-    }).otherwise({
-      redirectTo: '/'
-    });
-  });
+angular
+  .module('playlistApp')
+  .config(config)
 
-}).call(this);
+
+
+
+
 
 /*
 //@ sourceMappingURL=app.js.map
